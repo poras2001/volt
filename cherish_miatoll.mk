76a7d01acda0de5202e178ec1cad80df6171b8a4
poras2001 <91818021+poras2001@users.noreply.github.com>
2024-01-18 19:07:48 +0530

    Update and rename aosp_miatoll.mk to cherish_miatoll.mk

--- cherish_miatoll.mk
+++ cherish_miatoll.mk
@@ -9,19 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelBuilds stuff.
-$(call inherit-product, vendor/pb/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-
+# Cherish Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+EXTRA_UDFPS_ANIMATIONS := true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=OrouPorus
+CHERISH_BUILD_TYPE := unOFFICIAL
 
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := cherish_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
