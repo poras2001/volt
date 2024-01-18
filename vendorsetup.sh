0c65b642766435bc257b6bcd20d9fe63e10c5526
poras2001 <91818021+poras2001@users.noreply.github.com>
2024-01-18 19:21:59 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,22 @@
+[
+  {
+    "branch": "lineage-21",
+    "remote": "github",
+    "repository": "LineageOS/android_hardware_sony_timekeep",
+    "target_path": "hardware/sony/timekeep"
+  },
+  {
+    "remote": "pixelbuilds",
+    "repository": "platform_hardware_xiaomi",
+    "target_path": "hardware/xiaomi"
+  },
+  {
+    "repository": "platform_kernel_xiaomi_sm6250",
+    "target_path": "kernel/xiaomi/sm6250"
+  },
+  {
+    "remote": "pixelbuilds-blobs-gitlab",
+    "repository": "android_vendor_xiaomi_miatoll",
+    "target_path": "vendor/xiaomi/miatoll"
+  }
+]
