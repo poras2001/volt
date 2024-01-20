3e855261c06fb0102f2062acdeacb02c2c0eb1c2
poras2001 <91818021+poras2001@users.noreply.github.com>
2024-01-20 12:17:15 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,9 @@
 git clone https://github.com/clarencelol/vendor_xiaomi_miatoll vendor/xiaomi/miatoll
 
 git clone https://github.com/clarencelol/kernel_xiaomi_sm6250 kernel/xiaomi/sm6250
+
+git clone -b lineage-21 https://github.com/lineageOS/android_hardware_sony_timekeep hardware/sony/timekeep 
+
+rm -rf hardware/xiaomi
+
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
