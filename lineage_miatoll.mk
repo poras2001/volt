630a3f77d9ec596c774cb34ae49aaeb6e4fb6adc
poras2001 <91818021+poras2001@users.noreply.github.com>
2024-01-22 00:05:41 +0530

    Update and rename rising_miatoll.mk to lineage_miatoll.mk

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -9,21 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelBuilds stuff.
-$(call inherit-product, vendor/rising/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-# rising Flags
+# lineage Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP  := true
 EXTRA_UDFPS_ANIMATIONS := true
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.rising.maintainer=OrouPorus
-rising_BUILD_TYPE := unOFFICIAL
+    ro.lineage.maintainer=OrouPorus
+lineage_BUILD_TYPE := unOFFICIAL
 
-PRODUCT_NAME := rising_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
