96ba216b38bf557cd3e0b5f057b60d4ae46a8aa5
poras2001 <91818021+poras2001@users.noreply.github.com>
2024-01-22 16:53:53 +0530

    Update and rename lineage_miatoll.mk to leaf_miatoll.mk

--- leaf_miatoll.mk
+++ leaf_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2022 The leafOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,17 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelBuilds stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/leaf/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-# lineage Flags
+# leaf Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP  := true
 
-PRODUCT_NAME := lineage_miatoll
+PRODUCT_NAME := leaf_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
